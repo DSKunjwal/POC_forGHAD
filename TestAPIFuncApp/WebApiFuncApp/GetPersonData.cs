4d89fd523d37126c2f43c06cb72caf3f3589ea25
Devendra Singh Kunjwal <Devendra.Kumwal@halliburton.com>
2025-10-30 09:16:45 -0500

    added some code for sql testing

--- TestAPIFuncApp/WebApiFuncApp/GetPersonData.cs
+++ TestAPIFuncApp/WebApiFuncApp/GetPersonData.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.Data.SqlClient; // Replace System.Data.SqlClient with Microsoft.Data.SqlClient
 using System.Diagnostics;
 
 namespace WebApiFuncApp;
@@ -22,9 +23,30 @@ public class GetPersonData
         /// take user input
         Console.Write("Enter a filename to list: ");
         string userInput = Console.ReadLine();
-        
+        RunQuery(userInput);
         // shell command
         Process.Start("cmd.exe", "/C dir " + userInput);
         return new OkObjectResult("Welcome to Azure Functions!");
     }
+
+    public void RunQuery(string userInput)
+    {
+ 
+        string connectionString = "your_connection_string_here";
+        string query = "SELECT * FROM Products WHERE Name LIKE '" + userInput + "%'";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Name"]);
+            }
+        }
+    }
+
+
 }
